7e8dcd7ec32e4db6ac61d22c44908fc7c2554334
Kiefer Ares <kiefer_aresii@dlsu.edu.ph>
2020-06-09 10:23:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  print "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  print "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  print " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  print " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  print " #{board[6]} | #{board[7]} | #{board[8]} "
 end
